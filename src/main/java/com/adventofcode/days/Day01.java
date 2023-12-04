502a255ac79a2aafd242b5d942df72ccacd429cb
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-12-04 10:53:22 +0100

    Global code reformat

--- src/main/java/com/adventofcode/days/Day01.java
+++ src/main/java/com/adventofcode/days/Day01.java
@@ -1,6 +1,5 @@
 package com.adventofcode.days;
 
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,40 +9,43 @@ public class Day01 {
     public static void task1(String filePath) {
         List<String> lines = ReaderUtil.readLineByLineToList(filePath);
         List<String> cleanedLines = lines.stream().map(line -> line.replaceAll("[^0-9]", "")).toList();
-        int calibrationValuesSum = cleanedLines.stream().map(line -> String.valueOf(line.charAt(0)) + String.valueOf(line.charAt(line.length() - 1))).mapToInt(Integer::parseInt).sum();
+        int calibrationValuesSum = cleanedLines.stream()
+                .map(line -> String.valueOf(line.charAt(0)) + String.valueOf(line.charAt(line.length() - 1)))
+                .mapToInt(Integer::parseInt).sum();
         System.out.println(calibrationValuesSum);
     }
 
     public static void task2(String filePath) {
         List<String> lines = ReaderUtil.readLineByLineToList(filePath);
-        List<String> wordDigitsList = Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+        List<String> wordDigitsList = Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight",
+                "nine");
 
         List<String> cleanedLines = lines.stream().map(line -> {
             StringBuilder beginningBuilder = new StringBuilder();
             StringBuilder endBuilder = new StringBuilder();
             StringBuilder resultBuilder = new StringBuilder();
-            for(int i=0; resultBuilder.length() == 0; i++){
-                if(Character.isDigit(line.charAt(i))){
+            for (int i = 0; resultBuilder.length() == 0; i++) {
+                if (Character.isDigit(line.charAt(i))) {
                     resultBuilder.append(line.charAt(i));
                     break;
-                }else{
+                } else {
                     beginningBuilder.append(line.charAt(i));
                 }
                 wordDigitsList.stream().forEach(num -> {
-                    if(beginningBuilder.toString().contains(num)) {
+                    if (beginningBuilder.toString().contains(num)) {
                         resultBuilder.append(Integer.toString(wordDigitsList.indexOf(num) + 1));
                     }
                 });
             }
-            for(int i=line.length()-1; resultBuilder.length() == 1; i--){
-                if(Character.isDigit(line.charAt(i))){
+            for (int i = line.length() - 1; resultBuilder.length() == 1; i--) {
+                if (Character.isDigit(line.charAt(i))) {
                     resultBuilder.append(line.charAt(i));
                     break;
-                }else{
+                } else {
                     endBuilder.insert(0, line.charAt(i));
                 }
                 wordDigitsList.stream().forEach(num -> {
-                    if(endBuilder.toString().contains(num)) {
+                    if (endBuilder.toString().contains(num)) {
                         resultBuilder.append(Integer.toString(wordDigitsList.indexOf(num) + 1));
                     }
                 });
--- src/main/java/com/adventofcode/days/Day02.java
+++ src/main/java/com/adventofcode/days/Day02.java
@@ -7,19 +7,18 @@ import java.util.Map;
 import com.adventofcode.utils.ReaderUtil;
 
 public class Day02 {
-    public static void task1(String filePath){
+    public static void task1(String filePath) {
         String[] lines = ReaderUtil.readLineByLineToList(filePath).toArray(new String[0]);
         HashMap<String, Integer> maxCubesNumber = new HashMap<String, Integer>();
         maxCubesNumber.putAll(Map.of("red", 12, "green", 13, "blue", 14));
         int gamesSum = 0;
-        
-        outerLoop:
-        for(String line: lines){
+
+        outerLoop: for (String line : lines) {
             String[] splittedLine = line.split("[;,]\\s*|(:\\s)");
 
-            for(String cube : Arrays.copyOfRange(splittedLine, 1, splittedLine.length)){
+            for (String cube : Arrays.copyOfRange(splittedLine, 1, splittedLine.length)) {
                 String[] splittedCube = cube.split(" ");
-                if(Integer.parseInt(splittedCube[0]) > maxCubesNumber.get(splittedCube[1])){
+                if (Integer.parseInt(splittedCube[0]) > maxCubesNumber.get(splittedCube[1])) {
                     continue outerLoop;
                 }
             }
@@ -28,20 +27,20 @@ public class Day02 {
         System.out.println(gamesSum);
     }
 
-    public static void task2(String filePath){
+    public static void task2(String filePath) {
         String[] lines = ReaderUtil.readLineByLineToList(filePath).toArray(new String[0]);
         int gamesSum = 0;
-        for(String line : lines){
+        for (String line : lines) {
             String[] splittedLine = line.split("[;,]\\s*|(:\\s)");
             HashMap<String, Integer> maxCubesInGame = new HashMap<String, Integer>();
-            for(String cube : Arrays.copyOfRange(splittedLine, 1, splittedLine.length)){
+            for (String cube : Arrays.copyOfRange(splittedLine, 1, splittedLine.length)) {
                 String[] splittedCube = cube.split(" ");
-                
-                if(maxCubesInGame.containsKey(splittedCube[1])){
-                    if(maxCubesInGame.get(splittedCube[1]) < Integer.parseInt(splittedCube[0])){
+
+                if (maxCubesInGame.containsKey(splittedCube[1])) {
+                    if (maxCubesInGame.get(splittedCube[1]) < Integer.parseInt(splittedCube[0])) {
                         maxCubesInGame.put(splittedCube[1], Integer.parseInt(splittedCube[0]));
                     }
-                }else{
+                } else {
                     maxCubesInGame.put(splittedCube[1], Integer.parseInt(splittedCube[0]));
                 }
             }
