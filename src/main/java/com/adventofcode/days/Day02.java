458564601960906e4cd03c8a4dd3a56c4d226581
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-12-04 10:52:31 +0100

    part 2 for day 4 completed

--- src/main/java/com/adventofcode/days/Day02.java
+++ src/main/java/com/adventofcode/days/Day02.java
@@ -2,7 +2,6 @@ package com.adventofcode.days;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.adventofcode.utils.ReaderUtil;
--- src/main/java/com/adventofcode/days/Day04.java
+++ src/main/java/com/adventofcode/days/Day04.java
@@ -2,9 +2,9 @@ package com.adventofcode.days;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 import com.adventofcode.utils.ReaderUtil;
 
@@ -12,27 +12,13 @@ public class Day04 {
     public static void task1(String filePath) {
         List<String> lines = ReaderUtil.readLineByLineToList(filePath);
 
-        List<List<List<String>>> splittedLines = lines.stream().map(line -> Arrays.asList(line.split("\\s*\\|\\s*")))
-                .map(lineFragment -> lineFragment.stream().map(fragment -> Arrays.asList(fragment.trim().split("\\s+")))
-                        .map(innerList -> {
-                            if (innerList.get(0).equals("Card")) {
-                                return innerList.subList(2, innerList.size());
-                            }
-                            return innerList;
-                        })
-                        .toList())
-                .toList();
+        List<List<List<String>>> splittedLines = getSplittedLines(lines);
         List<Integer> points = new ArrayList<Integer>();
 
         splittedLines.stream().forEach(line -> {
-            AtomicInteger linePoints = new AtomicInteger(0);
-            line.get(1).stream().forEach(num -> {
-                if (line.get(0).contains(num)) {
-                    linePoints.incrementAndGet();
-                }
-            });
-            points.add(linePoints.get());
+            points.add(getPointsForLine(line));
         });
+
         AtomicInteger pointsSum = new AtomicInteger(0);
         points.forEach(point -> {
             if (point > 1) {
@@ -45,6 +31,47 @@ public class Day04 {
     }
 
     public static void task2(String filePath) {
+        List<String> lines = ReaderUtil.readLineByLineToList(filePath);
 
+        List<List<List<String>>> splittedLines = getSplittedLines(lines);
+        List<Integer> copiesNumbersList = new ArrayList<Integer>(Collections.nCopies(splittedLines.size(), 1));
+        for (int i = 0; i < splittedLines.size(); i++) {
+            List<List<String>> line = splittedLines.get(i);
+            int point = getPointsForLine(line);
+            copiesNumbersList = updateCopiesNumbersList(point, i, copiesNumbersList, copiesNumbersList.get(i));
+        }
+        System.out.println(copiesNumbersList.stream().mapToInt(Integer::intValue).sum());
+    }
+
+    private static List<List<List<String>>> getSplittedLines(List<String> lines) {
+        return lines.stream().map(line -> Arrays.asList(line.split("\\s*\\|\\s*")))
+                .map(lineFragment -> lineFragment.stream().map(fragment -> Arrays.asList(fragment.trim().split("\\s+")))
+                        .map(innerList -> {
+                            if (innerList.get(0).equals("Card")) {
+                                return innerList.subList(2, innerList.size());
+                            }
+                            return innerList;
+                        })
+                        .toList())
+                .toList();
+    }
+
+    private static List<Integer> updateCopiesNumbersList(int point, int currentLineNumber,
+            List<Integer> copiesNumbersList, int cardCopiesNumber) {
+        for (int i = 0; i < point; i++) {
+            int index = currentLineNumber + i + 1;
+            copiesNumbersList.set(index, copiesNumbersList.get(index) + cardCopiesNumber);
+        }
+        return copiesNumbersList;
+    }
+
+    private static int getPointsForLine(List<List<String>> line) {
+        AtomicInteger linePoints = new AtomicInteger(0);
+        line.get(1).stream().forEach(num -> {
+            if (line.get(0).contains(num)) {
+                linePoints.incrementAndGet();
+            }
+        });
+        return linePoints.get();
     }
 }
