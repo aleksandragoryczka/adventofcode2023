e4cdb8e34da3e1effaa69ca53c511d18d1d55f8b
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-12-06 09:41:39 +0100

    day6 part2 completed

--- src/main/java/com/adventofcode/days/Day06.java
+++ src/main/java/com/adventofcode/days/Day06.java
@@ -1,9 +1,9 @@
 package com.adventofcode.days;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.adventofcode.utils.ReaderUtil;
 
@@ -11,31 +11,43 @@ public class Day06 {
 
     public static void task1(String filePath) {
         List<String> lines = ReaderUtil.readLineByLineToList(filePath);
-        List<List<Integer>> numbersList = lines.stream()
+        List<List<Long>> numbersList = lines.stream()
                 .map(line -> Arrays.stream(line.split("\\s+|(?<=Time:)|(?<=Distance:)"))
-                        .filter(str -> str.matches("\\d+")).mapToInt(Integer::parseInt).boxed().toList())
+                        .filter(str -> str.matches("\\d+")).mapToLong(Long::parseLong).boxed().toList())
                 .toList();
-        List<Integer> timesList = numbersList.get(0);
-        List<Integer> distancesList = numbersList.get(1);
-        
+        countResults(numbersList.get(0), numbersList.get(1));
+    }
+
+    public static void task2(String filePath) {
+        List<String> lines = ReaderUtil.readLineByLineToList(filePath);
+        List<Long> numbersList = lines.stream()
+                .map(line -> Arrays.stream(line.trim().split("\\s+|(?<=Time:)|(?<=Distance:)"))
+                        .filter(str -> str.matches("\\d+"))
+                        .map(String::valueOf)
+                        .collect(Collectors.joining()))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+
+        countResults(Arrays.asList(numbersList.get(0)), Arrays.asList(numbersList.get(1)));
+
+    }
+
+    private static void countResults(List<Long> timesList, List<Long> distancesList) {
         List<Integer> resultsList = solveQuadricEquation(timesList, distancesList);
 
         int resultMultiplication = resultsList.stream().reduce(1, (a, b) -> a * b);
         System.out.println(resultMultiplication);
     }
 
-    public static void task2(String filePath) {
-    }
-
-    private static List<Integer> solveQuadricEquation(List<Integer> timesList, List<Integer> distancesList) {
+    private static List<Integer> solveQuadricEquation(List<Long> timesList, List<Long> distancesList) {
         List<Integer> resultsList = Arrays.asList(new Integer[timesList.size()]);
         Collections.fill(resultsList, 0);
         for (int i = 0; i < timesList.size(); i++) {
-            int time = timesList.get(i);
-            int distance = distancesList.get(i);
-            int f1 = time - 1;
+            long time = timesList.get(i);
+            long distance = distancesList.get(i);
+            long f1 = time - 1;
             int x1 = 0;
-            int x2 = time;
+            long x2 = time;
             double a = f1 / ((1 - x1) * (1 - x2));
 
             for (int x = 1; x < time; x++) {
