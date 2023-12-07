b9621bd33c5447013e118eb5388afbc97a912de2
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-12-07 12:23:59 +0100

    Day07 part2 completed

--- src/main/java/com/adventofcode/days/Day07.java
+++ src/main/java/com/adventofcode/days/Day07.java
@@ -1,7 +1,6 @@
 package com.adventofcode.days;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +14,30 @@ public class Day07 {
     public static void task1(String filePath) {
         List<String> lines = ReaderUtil.readLineByLineToList(filePath);
 
+        HashMap<List<Character>, Integer> handsMap = getHandsMapFromLines(lines);
+
+        List<List<HashMap<List<Character>, Integer>>> orderHandsList = getOrderHandsList(handsMap);
+        List<Integer> valuesInOrder = sortAndGetValues(orderHandsList, "AKQJT98765432");
+
+        int result = IntStream.range(0, valuesInOrder.size()).map(i -> valuesInOrder.get(i) * (i + 1)).sum();
+
+        System.out.println(result);
+    }
+
+    public static void task2(String filePath) {
+        List<String> lines = ReaderUtil.readLineByLineToList(filePath);
+
+        HashMap<List<Character>, Integer> handsMap = getHandsMapFromLines(lines);
+
+        List<List<HashMap<List<Character>, Integer>>> orderHandsList = getOrderHandsListWithJokers(handsMap);
+        List<Integer> valuesInOrder = sortAndGetValues(orderHandsList, "AKQT98765432J");
+
+        int result = IntStream.range(0, valuesInOrder.size()).map(i -> valuesInOrder.get(i) * (i + 1)).sum();
+
+        System.out.println(result);
+    }
+
+    private static HashMap<List<Character>, Integer> getHandsMapFromLines(List<String> lines) {
         HashMap<List<Character>, Integer> handsMap = new HashMap<List<Character>, Integer>();
         for (String line : lines) {
             String[] splittedLine = line.split(" ");
@@ -24,31 +47,82 @@ public class Day07 {
             }
             handsMap.put(key, Integer.parseInt(splittedLine[1]));
         }
-
-        List<List<HashMap<List<Character>, Integer>>> orderHandsList = getOrderHandsList(handsMap);
-        List<Integer> valuesInOrder = sortAndGetValues(orderHandsList);
-
-        int result = IntStream.range(0, valuesInOrder.size()).map(i -> valuesInOrder.get(i) * (i + 1)).sum();
-
-        System.out.println(result);
+        return handsMap;
     }
 
-    public static void task2(String filePath) {
-
-    }
-
-    private static List<Integer> sortAndGetValues(List<List<HashMap<List<Character>, Integer>>> orderHandsList) {
+    private static List<Integer> sortAndGetValues(List<List<HashMap<List<Character>, Integer>>> orderHandsList,
+            String keyChars) {
         for (List<HashMap<List<Character>, Integer>> list : orderHandsList) {
             if (list.size() > 1) {
                 Collections.sort(list, (o1, o2) -> {
                     return compareKeys(((HashMap<List<Character>, Integer>) o1).keySet().iterator().next(),
-                            ((HashMap<List<Character>, Integer>) o2).keySet().iterator().next());
+                            ((HashMap<List<Character>, Integer>) o2).keySet().iterator().next(), keyChars);
                 });
             }
         }
         List<Integer> valuesInOrder = orderHandsList.stream()
                 .flatMap(list -> list.stream().flatMap(map -> map.values().stream())).toList();
         return valuesInOrder;
+    }
+
+    private static List<List<HashMap<List<Character>, Integer>>> insertIntoOrderHandsList(int index,
+            List<List<HashMap<List<Character>, Integer>>> orderHandsList, List<Character> cards, int handValue) {
+        orderHandsList.get(index).add(new HashMap<List<Character>, Integer>(Map.of(cards, handValue)));
+        return orderHandsList;
+    }
+
+    private static List<List<HashMap<List<Character>, Integer>>> getOrderHandsListWithJokers(
+            HashMap<List<Character>, Integer> handsMap) {
+        List<List<HashMap<List<Character>, Integer>>> orderHandsList = new ArrayList<List<HashMap<List<Character>, Integer>>>();
+        for (int i = 0; i < 7; i++) {
+            orderHandsList.add(new ArrayList<HashMap<List<Character>, Integer>>());
+        }
+        for (Map.Entry<List<Character>, Integer> hand : handsMap.entrySet()) {
+            List<Character> cards = hand.getKey();
+            Map<Object, Long> charOccurences = cards.stream()
+                    .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+            if (charOccurences.entrySet().size() == 1) {
+                orderHandsList = insertIntoOrderHandsList(6, orderHandsList, cards, hand.getValue());
+            } else if (charOccurences.entrySet().size() == 2) {
+                if (charOccurences.values().stream().anyMatch(x -> x == 4)) {
+                    if (charOccurences.containsKey('J')) {
+                        orderHandsList = insertIntoOrderHandsList(6, orderHandsList, cards, hand.getValue());
+                    } else
+                        orderHandsList = insertIntoOrderHandsList(5, orderHandsList, cards, hand.getValue());
+                } else if (charOccurences.values().stream().anyMatch(x -> x == 3)) {
+                    if (charOccurences.containsKey('J')) {
+                        orderHandsList = insertIntoOrderHandsList(6, orderHandsList, cards, hand.getValue());
+                    } else
+                        orderHandsList = insertIntoOrderHandsList(4, orderHandsList, cards, hand.getValue());
+                }
+            } else if (charOccurences.entrySet().size() == 3) {
+                if (charOccurences.values().stream().anyMatch(x -> x == 3)) {
+                    if (charOccurences.containsKey('J')) {
+                        orderHandsList = insertIntoOrderHandsList(5, orderHandsList, cards, hand.getValue());
+                    } else
+                        orderHandsList = insertIntoOrderHandsList(3, orderHandsList, cards, hand.getValue());
+                } else {
+                    if (charOccurences.containsKey('J')) {
+                        if (charOccurences.get('J') == 2)
+                            orderHandsList = insertIntoOrderHandsList(5, orderHandsList, cards, hand.getValue());
+                        else if (charOccurences.get('J') == 1)
+                            orderHandsList = insertIntoOrderHandsList(4, orderHandsList, cards, hand.getValue());
+                    } else
+                        orderHandsList = insertIntoOrderHandsList(2, orderHandsList, cards, hand.getValue());
+                }
+            } else if (charOccurences.entrySet().size() == 4) {
+                if (charOccurences.containsKey('J')) {
+                    orderHandsList = insertIntoOrderHandsList(3, orderHandsList, cards, hand.getValue());
+                } else
+                    orderHandsList = insertIntoOrderHandsList(1, orderHandsList, cards, hand.getValue());
+            } else {
+                if (charOccurences.containsKey('J')) {
+                    orderHandsList = insertIntoOrderHandsList(1, orderHandsList, cards, hand.getValue());
+                } else
+                    orderHandsList = insertIntoOrderHandsList(0, orderHandsList, cards, hand.getValue());
+            }
+        }
+        return orderHandsList;
     }
 
     private static List<List<HashMap<List<Character>, Integer>>> getOrderHandsList(
@@ -62,32 +136,29 @@ public class Day07 {
             Map<Object, Long> charOccurences = cards.stream()
                     .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
             if (charOccurences.entrySet().size() == 1) {
-                orderHandsList.get(6).add(new HashMap<List<Character>, Integer>(Map.of(cards, hand.getValue())));
+                orderHandsList = insertIntoOrderHandsList(6, orderHandsList, cards, hand.getValue());
             } else if (charOccurences.entrySet().size() == 2) {
                 if (charOccurences.values().stream().anyMatch(x -> x == 4)) {
-                    orderHandsList.get(5).add(new HashMap<List<Character>, Integer>(Map.of(cards, hand.getValue())));
-                }
-                if (charOccurences.values().stream().anyMatch(x -> x == 3)) {
-                    orderHandsList.get(4).add(new HashMap<List<Character>, Integer>(Map.of(cards, hand.getValue())));
+                    orderHandsList = insertIntoOrderHandsList(5, orderHandsList, cards, hand.getValue());
+                } else if (charOccurences.values().stream().anyMatch(x -> x == 3)) {
+                    orderHandsList = insertIntoOrderHandsList(4, orderHandsList, cards, hand.getValue());
                 }
             } else if (charOccurences.entrySet().size() == 3) {
                 if (charOccurences.values().stream().anyMatch(x -> x == 3)) {
-                    orderHandsList.get(3).add(new HashMap<List<Character>, Integer>(Map.of(cards, hand.getValue())));
+                    orderHandsList = insertIntoOrderHandsList(3, orderHandsList, cards, hand.getValue());
                 } else {
-                    orderHandsList.get(2).add(new HashMap<List<Character>, Integer>(Map.of(cards, hand.getValue())));
+                    orderHandsList = insertIntoOrderHandsList(2, orderHandsList, cards, hand.getValue());
                 }
             } else if (charOccurences.entrySet().size() == 4) {
-                orderHandsList.get(1).add(new HashMap<List<Character>, Integer>(Map.of(cards, hand.getValue())));
+                orderHandsList = insertIntoOrderHandsList(1, orderHandsList, cards, hand.getValue());
             } else {
-                orderHandsList.get(0).add(new HashMap<List<Character>, Integer>(Map.of(cards, hand.getValue())));
+                orderHandsList = insertIntoOrderHandsList(0, orderHandsList, cards, hand.getValue());
             }
         }
         return orderHandsList;
     }
 
-    private static int compareKeys(List<Character> key1, List<Character> key2) {
-        String keyChars = "AKQJT98765432";
-
+    private static int compareKeys(List<Character> key1, List<Character> key2, String keyChars) {
         for (int i = 0; i < 5; i++) {
             char char1 = key1.get(i);
             char char2 = key2.get(i);
