30e13b1a961ff4ad5cc29202d2070b60dc292d1d
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-12-11 12:02:03 +0100

    Day11 part 2 completed

--- src/main/java/com/adventofcode/days/Day11.java
+++ src/main/java/com/adventofcode/days/Day11.java
@@ -10,27 +10,70 @@ public class Day11 {
 
     public static void task1(String filePath) {
         List<String> lines = ReaderUtil.readLineByLineToList(filePath);
-        List<List<Character>> newLines = doubleDotsColumn(doubleDotsRow(lines)).stream()
-                .map(line -> line.chars().mapToObj(c -> (char) c).toList())
+        List<List<Character>> charsMatrix = lines.stream().map(line -> line.chars().mapToObj(c -> (char) c).toList())
                 .toList();
-        List<List<Integer>> startCoordinateList = getStartIndexes(newLines);
-        countDistances(startCoordinateList);
+        List<List<Integer>> startCoordinateList = getStartIndexes(charsMatrix);
+        List<Integer> startExpandingRowsNumberList = expandRowMilionTimes(lines);
+        List<Integer> startExpandingColumnNumberList = expandColumnMilionTimes(lines);
+        countDistancesWithRates(startCoordinateList, startExpandingRowsNumberList, startExpandingColumnNumberList, 1);
 
     }
 
     public static void task2(String filePath) {
+        List<String> lines = ReaderUtil.readLineByLineToList(filePath);
+        List<List<Character>> charsMatrix = lines.stream().map(line -> line.chars().mapToObj(c -> (char) c).toList())
+                .toList();
+        List<List<Integer>> startCoordinateList = getStartIndexes(charsMatrix);
+        List<Integer> startExpandingRowsNumberList = expandRowMilionTimes(lines);
+        List<Integer> startExpandingColumnNumberList = expandColumnMilionTimes(lines);
+        countDistancesWithRates(startCoordinateList, startExpandingRowsNumberList, startExpandingColumnNumberList,
+                999999);
     }
 
-    private static int countDistances(List<List<Integer>> startCoordinateList) {
-        int sum = 0;
-        for (int i = 0; i < startCoordinateList.size(); i++) {
+    private static void countDistancesWithRates(List<List<Integer>> startCoordinateList,
+            List<Integer> startExpandingRowsNumberList, List<Integer> startExpandingColumnNumberList, int rate) {
+        long sum = 0;
+        for (int i = 0; i < startCoordinateList.size() - 1; i++) {
             for (int j = i + 1; j < startCoordinateList.size(); j++) {
-                sum += Math.abs(startCoordinateList.get(i).get(0) - startCoordinateList.get(j).get(0))
-                        + Math.abs(startCoordinateList.get(i).get(1) - startCoordinateList.get(j).get(1));
+
+                int x1 = startCoordinateList.get(i).get(0);
+                int y1 = startCoordinateList.get(i).get(1);
+                int x2 = startCoordinateList.get(j).get(0);
+                int y2 = startCoordinateList.get(j).get(1);
+
+                List<Integer> indexesRow = startExpandingRowsNumberList.stream()
+                        .filter(val -> (val > x1 && val < x2) || (val < x1 && val > x2))
+                        .toList();
+                List<Integer> indexesColumn = startExpandingColumnNumberList.stream()
+                        .filter(val -> (val < y1 && val > y2) || (val > y1 && val < y2)).toList();
+                long milionRateRows = indexesRow.size() > 0 ? indexesRow.size() * rate : 0;
+                long milionRateColumns = indexesColumn.size() > 0 ? indexesColumn.size() * rate : 0;
+
+                sum += Math.abs(x1 - x2) + milionRateRows + Math.abs(y1 - y2) + milionRateColumns;
             }
         }
         System.out.println(sum);
-        return sum;
+    }
+
+    private static List<Integer> expandRowMilionTimes(List<String> lines) {
+        List<Integer> startExpandingRowsNumberList = new ArrayList<Integer>();
+
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).chars().allMatch(c -> c == '.')) {
+                startExpandingRowsNumberList.add(i);
+            }
+        }
+        return startExpandingRowsNumberList;
+    }
+
+    private static List<Integer> expandColumnMilionTimes(List<String> lines) {
+        List<Integer> columnWithDotsIndex = new ArrayList<Integer>();
+        for (AtomicInteger i = new AtomicInteger(0); i.get() < lines.get(0).length(); i.incrementAndGet()) {
+            if (lines.stream().allMatch(line -> line.charAt(i.get()) == '.')) {
+                columnWithDotsIndex.add(i.get());
+            }
+        }
+        return columnWithDotsIndex;
     }
 
     private static List<List<Integer>> getStartIndexes(List<List<Character>> matrix) {
@@ -43,35 +86,5 @@ public class Day11 {
             }
         }
         return startCoordinateList;
-    }
-
-    private static List<String> doubleDotsRow(List<String> lines) {
-        List<String> newLines = new ArrayList<String>();
-        for (String line : lines) {
-            if (line.chars().allMatch(c -> c == '.')) {
-                newLines.add(line);
-                newLines.add(line);
-            } else {
-                newLines.add(line);
-            }
-        }
-        return newLines;
-    }
-
-    private static List<String> doubleDotsColumn(List<String> lines) {
-        List<Integer> columnWithDotsIndex = new ArrayList<Integer>();
-        for (AtomicInteger i = new AtomicInteger(0); i.get() < lines.get(0).length(); i.incrementAndGet()) {
-            if (lines.stream().allMatch(line -> line.charAt(i.get()) == '.')) {
-                columnWithDotsIndex.add(i.get());
-            }
-        }
-        for (int i = columnWithDotsIndex.size() - 1; i >= 0; i--) {
-            for (int j = 0; j < lines.size(); j++) {
-                StringBuilder rowBuilder = new StringBuilder(lines.get(j));
-                rowBuilder.insert(columnWithDotsIndex.get(i), ".");
-                lines.set(j, rowBuilder.toString());
-            }
-        }
-        return lines;
     }
 }
