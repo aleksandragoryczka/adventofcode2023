1ab99a759fae0e43fe9bf63ee553aa4fc3c4a787
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-12-12 21:35:34 +0100

    Day12 part2 completed

--- src/main/java/com/adventofcode/days/Day12.java
+++ src/main/java/com/adventofcode/days/Day12.java
@@ -1,12 +1,15 @@
 package com.adventofcode.days;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.adventofcode.utils.ReaderUtil;
 
 public class Day12 {
-    private static int recursionCounter = 0;
+
+    private static Map<String, Long> cache = new HashMap<String, Long>();
 
     public static void task1(String filePath) {
         List<String> lines = ReaderUtil.readLineByLineToList(filePath);
@@ -19,7 +22,7 @@ public class Day12 {
             List.of(lineSplitted[1].split(",")).stream().forEach(c -> countsList.add(Integer.parseInt(c)));
             counts.add(countsList);
         }
-        int sum = 0;
+        long sum = 0;
         for (int i = 0; i < codes.size(); i++) {
             String currentCode = codes.get(i);
             List<Integer> currentGroupsList = counts.get(i);
@@ -28,9 +31,35 @@ public class Day12 {
         System.out.println(sum);
     }
 
-    private static int recursiveCheckString(String code, List<Integer> groupsList) {
-        // System.out.println("code: " + code + " groupsList: " + groupsList);
-        int result = 0;
+    public static void task2(String filePath) {
+        List<String> lines = ReaderUtil.readLineByLineToList(filePath);
+        List<String> codes = new ArrayList<String>();
+        List<List<Integer>> counts = new ArrayList<List<Integer>>();
+        for (String line : lines) {
+            String[] lineSplitted = line.split(" ");
+            codes.add(lineSplitted[0] + "?" + lineSplitted[0] + "?" + lineSplitted[0] + "?" + lineSplitted[0] + "?"
+                    + lineSplitted[0]);
+            List<Integer> countsList = new ArrayList<Integer>();
+            for (int i = 0; i < 5; i++) {
+                List.of(lineSplitted[1].split(",")).stream().forEach(c -> countsList.add(Integer.parseInt(c)));
+            }
+            counts.add(countsList);
+        }
+        long sum = 0;
+        for (int i = 0; i < codes.size(); i++) {
+            String currentCode = codes.get(i);
+            List<Integer> currentGroupsList = counts.get(i);
+            sum += recursiveCheckString(currentCode, currentGroupsList);
+        }
+        System.out.println(sum);
+    }
+
+    private static long recursiveCheckString(String code, List<Integer> groupsList) {
+        long result = 0;
+        String key = code + groupsList.toString();
+        if(cache.containsKey(key)) {
+            return cache.get(key);
+        }
         if (code.isEmpty()) {
             return (groupsList.size() == 0) ? 1 : 0;
         }
@@ -52,11 +81,7 @@ public class Day12 {
                 }
             }
         }
-
+        cache.put(key, result);
         return result;
-    }
-
-    public static void task2(String filePath) {
-
     }
 }
